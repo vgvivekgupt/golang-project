3da80e762f678d818ff61114e0dc12a38b261ca0
Vivek gupta <vgvivekgupt@gmail.com>
2024-08-28 08:36:59 -0400

    second commit

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ const pageTemplate = `
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Beautiful Go Web Page</title>
+    <title>This is First My  Go Web Page</title>
     <style>
         body {
             font-family: Arial, sans-serif;
